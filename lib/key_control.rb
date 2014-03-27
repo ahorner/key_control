df142bcb12ae6316509ae3da01f4104fe03b3457
Andrew Horner <andrew@tablexi.com>
2014-03-26 19:48:25 -0500

    Document issues with thread and process keyrings

--- lib/key_control.rb
+++ lib/key_control.rb
@@ -3,10 +3,14 @@ require "key_control/key_ring"
 
 module KeyControl
 
-  THREAD  = "@t"
-  PROCESS = "@p"
   SESSION = "@s"
   USER    = "@u"
   DEFAULT = "@us"
   GROUP   = "@g"
+
+  # Thread and Process-level keyrings won't work for the time being, due to the
+  # fact that calls to keyctl have to happen through a subshell. These are here
+  # for the sake of documentation.
+  THREAD  = "@t"
+  PROCESS = "@p"
 end
